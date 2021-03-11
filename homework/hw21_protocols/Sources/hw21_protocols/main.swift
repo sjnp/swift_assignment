0e02c9a94c9c3edb6d1e201b4a5f0952611fade3
sjnp <sjnp@localhost.localdomain>
2021-03-12 01:39:26 +0700

    unfinished hw21

--- homework/hw21_protocols/Sources/hw21_protocols/main.swift
+++ homework/hw21_protocols/Sources/hw21_protocols/main.swift
@@ -1,1 +1,54 @@
-print("Hello, world!")
+// HW21: Protocols 
+//=================================================================================================================
+print("=============== Protocol syntax ===============")
+
+// protocol SomeProtocol {
+//     protocol definition goes here
+// }
+
+// Adopting a protocol
+// struc someStructure: someProtocol, anotherProtocol {
+//     structure defenition goes here
+// }
+
+print("=============== Protocol requirements ===============")
+// Error: property in protocol must have explicit { get } or { get set }
+// protocol NoNeedForGetOrSet {
+//     var someInt: Int
+//     var someString: String
+// }
+
+protocol ConstInt {
+    let someInt: Int { get }
+}
+protocol NoNeedForSet {
+    var someInt: Int { get }
+}
+protocol NeedGetAndSet {
+    var someInt: Int { get set }
+}
+
+class ConstIntA: NoNeedForSet {
+    var someInt: Int = 5
+}
+
+class ConstIntB: NeedGetAndSet {
+    var someInt: Int = 5
+}
+
+class ReadOnlyInt: NoNeedForSet {
+    var someDouble: Double
+    var someInt: Int { Int(someDouble) }    // Read-only computed properties
+    init(someDouble: Double) { self.someDouble = someDouble }
+}
+
+print("=============== Method requirements ===============")
+protocol Incrementing {
+    static var someInt: Int { get }
+    static func increment() -> ()
+}
+class IncrementIntA: Incrementing {
+    static var someInt: Int { return self.someInt }
+    static func increment() { someInt = someInt + 1 }
+    init(someInt: Int) { self.someInt = someInt }
+}
